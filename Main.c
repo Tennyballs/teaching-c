9996a335e94ee08dcadc72e3ff97f098487c7852
Cuppy130 <player130games@gmail.com>
2025-08-29 19:11:56 -0700

    teaching c

---
+++ Main.c
@@ -0,0 +1,20 @@
+// void -> 0/0
+
+// short (int / 2) -> a shorter whole number
+// int -> whole number
+// long (int*2) -> longer whole number
+
+// floats, doubles -> decimal (xxx.xxxxxx)
+
+// char (character) -> 0 to 255 values (256 unique values) (8 bits) or one character like 'a' (SINGLE CHARACTER)
+// char *variableName -> "My Name Is Michael Or something idk" (STRING / Array of characters)
+
+// rules for the main function
+// main function requires int at the start
+// it is to declare it is a "variable" that returns a value, in this case (int)
+// above 0 is error
+// return = 0 is a success
+int main()
+{
+    return 0;
+}
